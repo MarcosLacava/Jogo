4f28a5dc54e197af733414aa2430e5139975893a
MarcosLacava <marcoslaurean@gmail.com>
2022-07-06 19:32:41 -0300

    t7j8o

--- Player.py
+++ Player.py
@@ -20,6 +20,7 @@ class Player(pygame.sprite.Sprite):
         self.rect.center = (pos_inicial[1]*self.spritesheet.tileLen+self.spritesheet.tileLen/2, pos_inicial[0]*self.spritesheet.tileLen+self.spritesheet.tileLen/2)
         self.mapa = mapa
         self.mov_count = 0
+        self.anim_frame = 0
         
     def mover(self, posicao):
         # Move o sprite do Player
@@ -28,10 +29,12 @@ class Player(pygame.sprite.Sprite):
     def movimento(self):
         # Realiza o movimento do Player no mapa em tiles
         movimento = pygame.Vector2()
+        # Animação
 
         # Move o Player até a proxima tile
         if self.dir != (0,0):
             movimento += (self.dir[0]*self.velocidade, self.dir[1]*self.velocidade)
+            
             # Testa se o Player chegou na tile desejada
             if abs(self.rect.topleft[0] - self.pos[1]*self.spritesheet.tileLen) <= self.velocidade and abs(self.rect.topleft[1] - self.pos[0]*self.spritesheet.tileLen) <= self.velocidade:
                 self.rect.topleft = (self.pos[1]*self.spritesheet.tileLen, self.pos[0]*self.spritesheet.tileLen)    
@@ -47,18 +50,23 @@ class Player(pygame.sprite.Sprite):
         if teclas[pygame.K_w] and self.pos[0] != 0 and self.mapa[self.pos[0]-1][self.pos[1]] != 1:
             self.dir = (0,-1)
             self.pos = (self.pos[0]-1, self.pos[1])
+            self.image = self.spritesheet.cortar_sprite("frach_sprite05.png")
         elif teclas[pygame.K_s] and self.pos[0] != len(self.mapa)-1 and self.mapa[self.pos[0]+1][self.pos[1]] != 1:    
             self.dir = (0,1)
             self.pos = (self.pos[0]+1, self.pos[1])
+            self.image = self.spritesheet.cortar_sprite("frach_sprite01.png")
         elif teclas[pygame.K_a] and self.pos[1] != 0 and self.mapa[self.pos[0]][self.pos[1]-1] != 1:      
             self.dir = (-1,0)
             self.pos = (self.pos[0], self.pos[1]-1)
+            self.image = self.spritesheet.cortar_sprite("frach_sprite09.png")
         elif teclas[pygame.K_d] and self.pos[1] != len(self.mapa[0])-1 and self.mapa[self.pos[0]][self.pos[1]+1] != 1: 
             self.dir = (1,0)
             self.pos = (self.pos[0], self.pos[1]+1)
+            self.image = self.spritesheet.cortar_sprite("frach_sprite07.png")
 
     def update(self, *args: Any, **kwargs: Any) -> None:
         self.rect.center += self.movimento()
+        self.anim_frame 
         return super().update(*args, **kwargs)
 
     
