6912aaee3c58625038ca84eb916e2d12ba46e11a
MarcosLacava <marcoslaurean@gmail.com>
2022-07-10 17:32:42 -0300

    commit

--- Mapa.py
+++ Mapa.py
@@ -13,13 +13,40 @@ class Mapa():
         self.colisores = []
         self.matriz_mapa = mapa
 
-        # Adiciona tiles de transição baseado no mapa
-        for i in range(1, len(mapa)-1):
-            for j in range(0, len(mapa[0])):
-                if mapa[i][j] == 0 and i != 0 and i != len(mapa):
+        # Adiciona tiles de transição baseado na matriz do mapa
+        for i in range(0, self.largura):
+            for j in range(0, self.largura):
+                if mapa[i][j] == 0:
                     if mapa[i-1][j] == 1:
-                        mapa[i][j] = 4 
+                        if mapa[i][j-1] == 1:
+                            mapa[i][j] = 3    
+                        elif mapa[i][j+1] == 1:
+                            mapa[i][j] = 5
+                        else:
+                            mapa[i][j] = 4 
 
+                    elif mapa[i][j-1] == 1:
+                        if mapa[i+1][j] == 1:
+                            mapa[i][j] = 9
+                        else:
+                            mapa[i][j] = 10
+
+                    elif mapa[i][j+1] == 1:
+                        if mapa[i+1][j] == 1:
+                            mapa[i][j] = 7
+                        else:
+                            mapa[i][j] = 6
+                    
+                    elif mapa[i+1][j] == 1:
+                        mapa[i][j] = 8
+
+        # Troca paredes das extremidades por tiles de fundo
+        for i in range (self.largura):
+            mapa[0]            [i] = 2
+            mapa[i]            [0] = 2
+            mapa[self.largura-1][i] = 2
+            mapa[i][self.largura-1] = 2
+                    
         # Cria cada tile do mapa
         for i in range(len(mapa)):
             for j in range(len(mapa[i])):
