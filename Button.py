22d960ad0df887aa4023fb2a94ff04b484652450
oasjs <oasjs@users.noreply.github.com>
2022-07-14 12:52:17 -0300

    commit (button)

--- Button.py
+++ Button.py
@@ -4,19 +4,20 @@ import os
 class Button:
     def __init__(self, name, pos) -> None:
 
-        self.surf = pygame.image.load((os.path.join("Sprites","buttons", name + ".png")))
-        self.rect = self.surf.get_rect(center = (pos))
+        self.surf_default = pygame.image.load((os.path.join("Sprites","buttons", name + ".png")))
+        self.rect_default = self.surf_default.get_rect(center = (pos))
+
+        self.surf_hover = pygame.image.load((os.path.join("Sprites","buttons", name + "-hover.png")))
+        self.rect_hover = self.surf_hover.get_rect(center = (pos))
+
         self.name = name
 
     def blit_button(self, surf):
 
-        surf.blit(self.surf, self.rect)
+        surf.blit(self.surf_default, self.rect_default)
         mx, my = pygame.mouse.get_pos()
 
-        if self.rect.collidepoint((mx,my)):
-            self.surf = pygame.image.load((os.path.join("Sprites","buttons", self.name + "-hover.png")))
+        if self.rect_default.collidepoint((mx,my)):
+            surf.blit(self.surf_hover, self.rect_hover)
             return True
-        else:
-            self.surf = pygame.image.load((os.path.join("Sprites","buttons", self.name + ".png")))
-        
         return False
